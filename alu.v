cbf4b54ec1c3821fa1ecbb467be7decca2c2fc93
Adrian Parvin Ouano <me@adrianpar.vin>
2023-08-07 19:33:23 +0800

    Implement logic

--- alu.v
+++ alu.v
@@ -1,13 +1,22 @@
 module alu(
     input [31:0] a,
     input [31:0] b,
-    output [31:0] d,
+    output reg [31:0] d,
     
     input [2:0] op
 );
 
 wire sub = op == 3'b100;
-wire [31:0] b_ = !sub ? b : ~b;
-assign d = a + b_ + (sub ? 'b1 : 'b0);
+wire [31:0] b_ = sub ? ~b : b;
+wire [31:0] c_ = sub ? 'b1 : 'b0;
+
+always @* begin
+    case (op[1:0])
+        0: d = a + b_ + c_;
+        1: d = a & b;
+        2: d = a | b;
+        3: d = a ^ b;
+    endcase
+end
 
 endmodule
