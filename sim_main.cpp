373899d56bdbb8b8affdfc0f6abe0b1ad574440d
Adrian Parvin Ouano <me@adrianpar.vin>
2023-08-08 03:14:59 +0800

    Add fibonacci test and also test subtraction

--- sim_main.cpp
+++ sim_main.cpp
@@ -5,11 +5,23 @@
 
 int main(int argc, char** argv) {
   uint32_t rom[16] = {0};
+  rom[0] = 0x40000093;
+  rom[1] = 0x00000113;
+  rom[2] = 0x00100193;
+  rom[3] = 0x00310133;
+  rom[4] = 0x0020a023;
+  rom[5] = 0x002181b3;
+  rom[6] = 0x0030a023;
+  rom[7] = 0xfe0008e3;
+
   rom[0] = 0x40000113; // li	sp,1024
   rom[1] = 0x01000193; // li	gp,16
   rom[2] = 0x00110023; // sb	ra,0(sp)
   rom[3] = 0x00108093; // add	ra,ra,1
   rom[4] = 0xfe309ce3; // bne	ra,gp,8 <.L1^B1>
+  rom[5] = 0x401080b3;
+  rom[6] = 0x00110023;
+  rom[7] = 0x00000063;
 
   VerilatedContext* contextp = new VerilatedContext;
   contextp->commandArgs(argc, argv);
@@ -20,7 +32,8 @@ int main(int argc, char** argv) {
 
     if (top->clk && top->mem && !top->mem_read) {
       if (top->addr == 1024) {
-        printf("data=%x, addr=%x\n", top->data, top->addr);
+        printf("data=%010d(0x%08x), addr=%x\n", top->data, top->data, top->addr);
+        fflush(stdout);
       }
     }
 
