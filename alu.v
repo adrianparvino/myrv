be8c3326032f82d0c594ee68c3995ffc9e5f1ed5
Adrian Parvin Ouano <me@adrianpar.vin>
2023-08-10 05:29:29 +0800

    Update d and d2 simultaneously

--- alu.v
+++ alu.v
@@ -14,26 +14,22 @@ module alu(
     input alt2_op
 );
 
-wire [31:0] b_ = alt_op ? ~b : b;
-wire [31:0] c_ = alt_op ? 'b1 : 'b0;
+reg ignore;
 
-wire [31:0] signed_bias = alt2_op ? 32'h80000000 : 0;
-reg [31:0] ignore;
+wire [31:0] arith_result = alt_op ? a - b : a + b;
 
 always @* begin
     case (alu_op)
-        0: d = a + b_ + c_;
-        1: d = a & b_;
-        2: d = a ^ b_;
-        3: d = a | b_;
+        0: d = arith_result;
+        1: d = a & b;
+        2: d = a ^ b;
+        3: d = a | b;
     endcase
-end
 
-always @* begin
     case (alu2_op)
         0: d2 = a2 << b2[4:0];
-        1: d2 = {31'b0, (signed_bias ^ a2) < (signed_bias ^ b2)};
-        2: {ignore, d2} = {{32{alt2_op & a2[31]}}, a2} >> b2[4:0];
+        1: d2 = {31'b0, ({alt2_op ^ a2[31], a2[30:0]} < {alt2_op ^ b2[31], b2[30:0]})};
+        2: {ignore, d2} = {alt2_op & a2[31], a2} >>> b2[4:0];
         3: d2 = b2;
     endcase
 end
